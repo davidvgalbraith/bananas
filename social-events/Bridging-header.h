1b0a7cf642c99b1aefea28ad42e9c20738264037
Dave <dave@jut.io>
2015-08-02 01:32:35 -0700

    Initial commit (Facebook integrated)

---
+++ social-events/Bridging-header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-header.h
+//  social-events
+//
+//  Created by David Galbraith on 8/2/15.
+//  Copyright (c) 2015 David Galbraith. All rights reserved.
+//
+
+#ifndef social_events_Bridging_header_h
+#define social_events_Bridging_header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif
